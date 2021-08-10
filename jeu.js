42e9048c594d9fdb47ee1e2e96a43cda7712cd8f
Jocelyne59 <mirandejocelyne@gmail.com>
2021-08-10 15:47:47 +0200

    new

---
+++ jeu.js
@@ -0,0 +1,16 @@
+
+ // tableau des mots à deviner
+let mot = ["ajout","basta","douce","final","jadis","ligne","peuls"];
+// nombre de tentatives
+let numberoftests = 8; 
+
+//choisir un mot 
+var random = Math.floor(Math.random()*mot.length);
+console.log(mot[random])
+
+// saisie de la lettre 
+
+let letter = "";
+
+
+ 
